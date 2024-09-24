0d338d04394ec2d8f829d10f99a934976727ce2f
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-24 09:12:36 -0400

    Add project files.

---
+++ Summative Casino Dice Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Summative_Casino_Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
